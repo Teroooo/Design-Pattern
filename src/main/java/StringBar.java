0ce6250204e48b644cd0fe27b4958fd783c355b0
Terooo <up202204971@up.pt>
2023-11-10 18:40:27 +0000

    3.

---
+++ src/main/java/StringBar.java
@@ -0,0 +1,20 @@
+public class StringBar extends Bar{
+    private boolean isHappyHour;
+
+    @Override
+    public boolean isHappyHour() {
+        return isHappyHour;
+    }
+
+    @Override
+    public void startHappyHour() {
+        isHappyHour = true;
+        notifyObservers();
+    }
+
+    @Override
+    public void endHappyHour() {
+        isHappyHour = false;
+        notifyObservers();
+    }
+}
